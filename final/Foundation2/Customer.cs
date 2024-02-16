8c31713ef8f15e83eaa51dae9e6f75e3f1db28cc
GrantBYUI <grantherman998@yahoo.com>
2024-02-16 15:15:26 -0700

    Learning Activity 3 Push to Github for Brother Himmer

--- final/Foundation2/Customer.cs
+++ final/Foundation2/Customer.cs
@@ -1,7 +1,17 @@
 using System;
+using System.Collections.Concurrent;
 
-public Class customer
+public Class Customer
 {
+   public string = get(); getname;
+   public get(); getaddress;
+
+   public Class Address
+
+
+    return Address;
+    return Country; // USA or not
+    
     // The customer contains a name and an address.
     // The name is a string, but the Address is a class.
     // The customer should have a method that can return whether they live in the USA or not. (Hint this should call a method on the address to find this.)
--- final/Foundation2/Order.cs
+++ final/Foundation2/Order.cs
@@ -1,7 +1,64 @@
 using System;
+using System.ComponentModel;
+using System.Net.Http.Headers;
+using System.Net.Sockets;
 
 public Class Order
 {
+
+public class Program
+{
+    //Attribute
+    public static void Main(string[] args)
+    {
+        List<Order> orders = new List<orders>();
+        Order reference = new Order();
+        Product reference = new Product();
+        AddNewOrder = new order ();
+        AddNewAddress = new Address();
+        Customer.Add(address);
+        Customer = new Customer();
+        Customer.Add(customer);
+        order = new order();
+        AddNewCustomer = new Customer();
+        Customer.Add(Customer);
+        Console.WriteLine("customer");
+        Console.WriteLine ("order");
+        Console.Write("please enter your order here");
+        Console.ReadLine();
+        Console.WriteLine("the scripture is {Order}");
+        Console.WriteLine();
+        //while (order.IsCompetelyHidden()==false);
+        Console.ReadLine();
+        Console.WriteLine("press enter to continue or type quit to finish:");
+        return;
+    }
+
+    // Method
+     //public List Addreference(reference _scripturereference)
+     //{
+        //Reference reference = new Reference("John", 3, 16);
+       // Scripture scripture = new Scripture(reference, "For God so loved the world, that he gave his only begotten Son, that whosoever believeth in him should not perish but have everlasting life.");
+        // Reference reference = new Reference("Proverbs", 3, 5-6);
+        // Scripture scripture = new Scripture(referance, "Trust in the Lord with all thine heart; and lean unto thine own understanding");
+        // Scripture scripture = new Scripture(referance, "In all thy ways acknowledge him, and he shall direct thy paths");
+            // while (scripture.IsCompletelyHidden()==true){}
+            // if (userInput.toHigh= "display");
+            // Console.WriteLine(scripture.GetDisplayText);
+
+            // while (scripture.IsCompetelyHidden()==false);
+            // Console.Clear();
+            // Console.WriteLine("Press Enter to hide more words or type 'quit' to exit:");
+            // string userInput = Console.ReadLine();
+            // if (userInput.ToLow() == "quit")
+            // {
+            //     break;
+            // }
+
+            // scripture.HideRandomWords(2);
+
+     //}
+}
     // Contains a list of products and a customer. Can calculate the total cost of the order. Can return a string for the packing label. Can return a string for the shipping label.
     // The total price is calculated as the sum of the total cost of each product plus a one-time shipping cost.
     // This company is based in the USA. If the customer lives in the USA, then the shipping cost is $5. If the customer does not live in the USA, then the shipping cost is $35.
