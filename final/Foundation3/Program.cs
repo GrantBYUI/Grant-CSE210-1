7bdc7c86cea0dddd9554b6e48911bf89baed4923
GrantBYUI <grantherman998@yahoo.com>
2024-02-20 10:59:57 -0700

    Learning Activity 3 Push to Github for Brother Himmer

--- final/Foundation3/Program.cs
+++ final/Foundation3/Program.cs
@@ -5,56 +5,20 @@ private Base Class Event
 {
 private static void Main(string[] args)
     {
-        List<activity> activities = new List<activity>();
-        Activity activity = new activity();
-        Summary summary = new summary(summary, "For God so loved the world, that he gave his only begotten Son, that whosoever believeth in him should not perish but have everlasting life.");
-        activity.Add(activity);
-        summary = new Summary();
-        activity = new Activity();
-        summary.Add(summary);
-        Console.WriteLine("activity");
-        Console.WriteLine("summary");
-        Console.WriteLine ("activity");
-        Console.Write("please enter your activity here ->");
-        Console.WriteLine(please enter your summary here -> ");
+        List<Event> event = new List<event>();
+        event Event = new event();
+        event = new Event();
+        event.Add(event);
+        
+        Console.Write("please enter your event here ->");
+        
         Console.ReadLine();
-        Console.WriteLine("the activity is {}");
+        Console.WriteLine("the event is {}");
         Console.WriteLine();
-        //while (activity.IsCompetelyHidden()==false);
-        // while (summary.IsCompletelyHidden()==false);
+        //while (event.IsCompetelyHidden()==false);
+        
         Console.ReadLine();
         Console.WriteLine("press enter to continue or type quit to finish:");
-        return;
-    } */
-}
-private class Program
-{
-    //Attribute
-     private static void Main(string[] args)
-    {
-        List<activity> activities = new List<activity>();
-        Activity activity = new activity();
-        Summary summary = new summary(summary, "For God so loved the world, that he gave his only begotten Son, that whosoever believeth in him should not perish but have everlasting life.");
-        activity.Add(activity);
-        summary = new Summary();
-        activity = new Activity();
-        summary.Add(summary);
-        Console.WriteLine("activity");
-        Console.WriteLine("summary");
-        Console.WriteLine ("activity");
-        Console.Write("please enter your activity here ->");
-        Console.WriteLine(please enter your summary here -> ");
-        Console.ReadLine();
-        Console.WriteLine("the activity is {}");
-        Console.WriteLine(");
-        //while (activity.IsCompetelyHidden()==false);
-        // while (summary.IsCompletelyHidden()==false);
-        Console.ReadLine();
-        Console.WriteLine("press enter to continue or type quit to finish:");
-        return;
-    }
-
-    // Method
-    
-
+        return event;
+    } 
 }
